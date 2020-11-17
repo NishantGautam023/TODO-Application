a8111783524e55851d14ac88b8ee2bb4a36607bc
Nishant Gautam <eddienishant@gmail.com>
2020-11-17 17:56:57 +0530

    ta help

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -6,31 +6,44 @@ const TODO = require('../models/todo_schema')
 // home page 
 
 module.exports.load = function (req, res) {
-    TODO.find({}, (err, list) => {
-        if (err) {
-            console.log('there is an error', err);
-            return;
+    // TODO.find({}, (err, list) => {
+    //     if (err) {
+    //         console.log('there is an error', err);
+    //         return;
+    //     }
+    //     return res.render('home', {
+    //         title: 'Home',
+    //         todo_list: list
+    //     })   
+    // });
+    TODO.find({}, function(err, list){
+        if(err){
+            console.log('Error!', err);
         }
-        return res.render('home', {
+
+        return res.render('home',{
             title: 'Home',
-            todo_list: list
-        })   
-    });
+            list: list
+        })
+        // try to refresh now
+        //open chrome
+    })
 };  
 
 // Creating our Task ..i.e put data in database
 module.exports.creating = function(req, res) {
     TODO.create( {
         task: req.body.task,
-         dueDate: req.body.dueDate,
+         dueDate: req.body.date,
          category: req.body.category,
+         // sir u there , can i call u ?one sec
     },
     // whenever we create something we need, to callback a function which tells 
     // whether an error is there,or has it been created(if created access it)
      function(err, newTODO)  {
          //1st argument is err and 2nd we can name anything
          if(err) {
-             console.log("Error in creating a TODO");
+             console.log("Error in creating a TODO",err);
              return;
          }
          // if created ...
